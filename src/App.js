a9a6f97d7306ee361fa651418a312af867d5ed9a
Ohm Shankar <167038152+AlbertEinstein07@users.noreply.github.com>
2025-05-19 21:59:14 -0500

    footer changes + test article

--- src/App.js
+++ src/App.js
@@ -153,6 +153,17 @@ const posts = [
       <p>Tucson's comprehensive approach combines rebates, education, and rainwater harvesting incentives. Since implementing these policies, the city has seen a 30% reduction in per capita water use despite population growth.</p>
     `,
   },
+  {
+    id: 4,
+    title: "Test Article",
+    date: "March 25, 2025",
+    summary:
+      "This is a test article, no need to worry...",
+    content:`
+      <p>this is a test article, testing out tags</p>
+      <h2>test header</h2>
+    `,
+  },
 ];
 
 function HeaderComponent() {
@@ -420,7 +431,7 @@ function FooterComponent() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 mt-0.5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span>(555) 123-4567</span>
+                <span>(512) 521-4661</span>
               </li>
             </ul>
           </div>
